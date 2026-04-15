c5bb4a8cdaa5156c751ace290059bdc95c6da104
Ollie Read <code@ollieread.com>
2026-04-15 17:55:46 +0100

    feat: add forge module with @Mod entry point and mods.toml

---
+++ forge/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    java
+    id("net.minecraftforge.gradle") version "[6.0,6.2)"
+    alias(libs.plugins.shadow)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+configure<net.minecraftforge.gradle.userdev.UserDevExtension> {
+    mappings("official", "1.20.1")
+
+    runs {
+        create("server") {
+            workingDirectory(project.file("run"))
+            args("--nogui")
+            mods {
+                create("cartograph") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    minecraft("net.minecraftforge:forge:1.20.1-47.3.0")
+    implementation(project(":common"))
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("cartograph-forge")
+    archiveClassifier.set("")
+}
+
+// Point reobfJar at the shadow JAR so the final artifact is reobfuscated
+afterEvaluate {
+    val reobfJar = tasks.named("reobfJar", net.minecraftforge.gradle.userdev.tasks.RenameJarInPlace::class)
+    reobfJar.configure {
+        input.set(tasks.shadowJar.get().archiveFile)
+    }
+    tasks.shadowJar {
+        finalizedBy(reobfJar)
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
---
+++ forge/src/main/java/gg/cartograph/plugin/forge/CartographForgeMod.java
@@ -0,0 +1,29 @@
+package gg.cartograph.plugin.forge;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("cartograph")
+public class CartographForgeMod {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public CartographForgeMod() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("Cartograph enabled (Forge)");
+    }
+
+    @SubscribeEvent
+    public void onServerStopping(ServerStoppingEvent event) {
+        LOGGER.info("Cartograph disabled (Forge)");
+    }
+}
