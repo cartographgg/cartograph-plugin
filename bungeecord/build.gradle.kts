ef970cc6782a7260ae7fea8fa70f08c2bfd94203
Ollie Read <code@ollieread.com>
2026-04-15 17:43:17 +0100

    feat: add bungeecord module with entry point and bungee.yml

---
+++ bungeecord/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.shadow)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
+}
+
+repositories {
+    maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
+    maven("https://libraries.minecraft.net/")
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    compileOnly(libs.bungeecord.api)
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("cartograph-bungeecord")
+    archiveClassifier.set("")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
---
+++ bungeecord/src/main/java/gg/cartograph/plugin/bungeecord/CartographBungeePlugin.java
@@ -0,0 +1,16 @@
+package gg.cartograph.plugin.bungeecord;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class CartographBungeePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Cartograph enabled (BungeeCord)");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Cartograph disabled (BungeeCord)");
+    }
+}
