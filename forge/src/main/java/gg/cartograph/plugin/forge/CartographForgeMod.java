f87e2a5be47a0e3d97563c36e9862be0f5b96ac2
Ollie Read <code@ollieread.com>
2026-04-17 12:48:14 +0100

    feat: add Forge config loader with ForgeConfigSpec

--- forge/src/main/java/gg/cartograph/plugin/forge/CartographForgeMod.java
+++ forge/src/main/java/gg/cartograph/plugin/forge/CartographForgeMod.java
@@ -1,10 +1,13 @@
 package gg.cartograph.plugin.forge;
 
+import gg.cartograph.plugin.common.config.CartographConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,14 +17,18 @@ public class CartographForgeMod
 
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private CartographConfig cartographConfig;
+
     public CartographForgeMod()
     {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ForgeConfigLoader.SPEC);
     }
 
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
+        cartographConfig = ForgeConfigLoader.load();
         LOGGER.info("Cartograph enabled (Forge)");
     }
 
@@ -29,5 +36,10 @@ public class CartographForgeMod
     public void onServerStopping(ServerStoppingEvent event)
     {
         LOGGER.info("Cartograph disabled (Forge)");
+    }
+
+    public CartographConfig getCartographConfig()
+    {
+        return cartographConfig;
     }
 }
